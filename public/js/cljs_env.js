ede92e5c7b9bab72efa00d9f4fb4645ec651c90d
Noah Moss <noahbmoss@gmail.com>
2025-03-08 22:24:16 -0500

    progress towards adding shadow-cljs

---
+++ public/js/cljs_env.js
@@ -0,0 +1,7 @@
+var $CLJS = module.exports = {};
+/*
+
+ Copyright The Closure Library Authors.
+ SPDX-License-Identifier: Apache-2.0
+*/
+'use strict';$CLJS.aa="closure_uid_"+(1E9*Math.random()>>>0);
---
+++ src/cljs/solve.cljc
@@ -0,0 +1,5 @@
+(ns solve)
+
+(defn ^:export solve-puzzle
+  [grid]
+  (str "Solving puzzle: " grid))
