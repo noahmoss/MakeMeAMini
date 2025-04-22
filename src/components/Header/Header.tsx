80e9d5db21e422fb870f7cac3d5b4bee9e1bcbc7
Noah Moss <noahbmoss@gmail.com>
2025-04-21 21:42:52 -0400

    responsive header menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,92 @@
-import { Play, Edit3, Settings, Link, HelpCircle } from "react-feather";
+import { MouseEventHandler, ReactNode } from "react";
+
+import { Play, Edit3, Settings, Link, HelpCircle, Tool } from "react-feather";
 import { useDisclosure } from "@mantine/hooks";
-import { Button, Tooltip } from "@mantine/core";
+import { Button, Tooltip, Burger, Drawer } from "@mantine/core";
 
 import Logo from "../Logo";
 import SettingsModal, { SettingsProps } from "../SettingsModal";
-
 import styles from "./Header.module.css";
 import { Mode } from "../Game";
+
+type ActionButtonProps = {
+  label: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>,
+  children?: ReactNode,
+}
+
+function ActionButton({ label, onClick, children }: ActionButtonProps) {
+  return (
+    <Tooltip label={label} withArrow={true} className={styles.tooltip}>
+      <Button
+        variant="subtle"
+        color="black"
+        size="compact-xl"
+        className={styles.actionButton}
+        onClick={onClick}
+        fullWidth={true}
+      >
+        {children}
+      </Button>
+    </Tooltip>
+  )
+}
+
+type HeaderActionsProps = {
+  mode: Mode;
+  setMode: (mode: Mode) => void;
+  openSettings: () => void;
+};
+
+function HeaderActions({ mode, setMode, openSettings }: HeaderActionsProps) {
+  return (
+    <div className={styles.iconGroup}>
+      {mode === "editing" ? (
+        <ActionButton label="Test solve" onClick={() => setMode("solving")} >
+          <div className={styles.iconAndLabel}>
+            <Edit3 />
+            <div className={styles.actionButtonLabel}>
+              Test solve
+            </div>
+          </div>
+        </ActionButton>
+      ) : (
+        <ActionButton label="Edit grid" onClick={() => setMode("editing")} >
+          <div className={styles.iconAndLabel}>
+            <Tool />
+            <div className={styles.actionButtonLabel}>
+              Edit
+            </div>
+          </div>
+        </ActionButton>
+      )}
+      <ActionButton label="Settings" onClick={openSettings} >
+        <div className={styles.iconAndLabel}>
+          <Settings />
+          <div className={styles.actionButtonLabel}>
+            Settings
+          </div>
+        </div>
+      </ActionButton>
+      <ActionButton label="Share">
+        <div className={styles.iconAndLabel}>
+          <Link />
+          <div className={styles.actionButtonLabel}>
+            Share
+          </div>
+        </div>
+      </ActionButton>
+      <ActionButton label="Help">
+        <div className={styles.iconAndLabel}>
+          <HelpCircle />
+          <div className={styles.actionButtonLabel}>
+            Help
+          </div>
+        </div>
+      </ActionButton>
+    </div>
+  )
+}
 
 type HeaderProps = {
   settingsProps: SettingsProps;
@@ -14,63 +94,55 @@ type HeaderProps = {
   setMode: (mode: Mode) => void;
 };
 
-function Header({ settingsProps, mode, setMode }: HeaderProps) {
-  const [settingsOpen, { open, close }] = useDisclosure(false);
-
-  const modeButtonTooltip =
-    mode === "editing" ? "Solve crossword" : "Edit crossword";
+function Header({ settingsProps, setMode, ...delegated }: HeaderProps) {
+  const [burgerOpen, { toggle: toggleBurger }] = useDisclosure(false);
+  const [settingsOpen, { open: openSettings, close: closeSettings }] = useDisclosure(false);
 
   return (
     <>
       <SettingsModal
         isOpen={settingsOpen}
-        onClose={close}
+        onClose={closeSettings}
         settingsProps={settingsProps}
       />
       <div className={styles.siteHeader}>
         <Logo />
-        <div className={styles.iconGroup}>
-          <Tooltip label={modeButtonTooltip} withArrow={true}>
-            {mode === "editing" ? (
-              <Button
-                variant="subtle"
-                color="black"
-                size="compact-xl"
-                onClick={() => setMode("solving")}
-              >
-                <Play />
-              </Button>
-            ) : (
-              <Button
-                variant="subtle"
-                color="black"
-                size="compact-xl"
-                onClick={() => setMode("editing")}
-              >
-                <Edit3 />
-              </Button>
-            )}
-          </Tooltip>
-          <Tooltip label="Settings" withArrow={true}>
-            <Button
-              variant="subtle"
-              color="black"
-              size="compact-xl"
-              onClick={open}
-            >
-              <Settings />
-            </Button>
-          </Tooltip>
-          <Tooltip label="Share" withArrow={true}>
-            <Button variant="subtle" color="black" size="compact-xl">
-              <Link />
-            </Button>
-          </Tooltip>
-          <Tooltip label="Help" withArrow={true}>
-            <Button variant="subtle" color="black" size="compact-xl">
-              <HelpCircle />
-            </Button>
-          </Tooltip>
+        <Burger opened={burgerOpen} onClick={toggleBurger} className={styles.hamburgerMenu} />
+        {/* Small screens - use a drawer */}
+        <Drawer
+          position="right"
+          opened={burgerOpen}
+          withOverlay={false}
+          withCloseButton={false}
+          onClose={toggleBurger}
+          styles={{
+            inner: {
+              marginTop: "calc(var(--header-height) - 4px)",
+            }
+          }}
+          className={styles.sidebar}
+        >
+          <div className={styles.mobileHeaderActionsWrapper}>
+            <HeaderActions
+              {...delegated}
+              setMode={(mode: Mode) => {
+                toggleBurger();
+                setMode(mode);
+              }}
+              openSettings={() => {
+                toggleBurger();
+                openSettings();
+              }}
+            />
+          </div>
+        </Drawer>
+        {/* Large screens - actions are in the header */}
+        <div className={styles.desktopHeaderActionsWrapper}>
+          <HeaderActions
+            openSettings={openSettings}
+            setMode={setMode}
+            {...delegated}
+          />
         </div>
       </div>
     </>
